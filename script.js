a6256467729c5da163a4f03a8f2425aca31e02eb
magnus madsen <magn@stud.kea.dk>
2023-11-11 12:43:32 +0100

    logo ani

--- script.js
+++ script.js
@@ -1,56 +1,77 @@
-const section1 = document.querySelector(".sec1")
-const section2 = document.querySelector(".sec2")
-const section3 = document.querySelector(".sec3")
-const section4 = document.querySelector(".sec4")
+const section1 = document.querySelector(".sec1");
+const section2 = document.querySelector(".sec2");
+const section3 = document.querySelector(".sec3");
+const section4 = document.querySelector(".sec4");
 
-window.addEventListener("load",aktiver);
-function aktiver(){
-    document.querySelector("ul").classList.add("aktiv")
-    document.querySelector("h1").classList.add("aktiv")
+window.addEventListener("load", aktiver);
+function aktiver() {
+  document.querySelector("h1").classList.add("aktiv");
+  document.querySelector(".linavn").classList.add("aktiv");
+  document.querySelector(".liaar").classList.add("aktiv");
 }
 window.addEventListener("scroll", reveal);
 function reveal() {
-    let reveals = document.querySelectorAll(".secani1");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 5;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("toright");
-       
-      }
+  let reveals = document.querySelectorAll(".secani1");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 5;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("toright");
     }
-    
   }
-  reveal();
+}
+reveal();
 window.addEventListener("scroll", reveal2);
 function reveal2() {
-    let reveals = document.querySelectorAll(".secani2");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 5;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("toleft");
-        
-      } 
+  let reveals = document.querySelectorAll(".secani2");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 5;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("toleft");
     }
-    
   }
-  reveal2();
+}
+reveal2();
 
 window.addEventListener("scroll", reveal3);
 function reveal3() {
-    let reveals = document.querySelectorAll(".footerani");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 5;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("totop");
-        
-      } 
+  let reveals = document.querySelectorAll(".footerani");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 5;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("totop");
     }
-    
   }
-  reveal3();
+}
+reveal3();
+
+const cls = document.querySelector(".cls-2");
+const cls3 = document.querySelector(".cls-3");
+const rect = document.querySelectorAll("rect");
+const path = document.querySelectorAll("path");
+
+cls.addEventListener("animationend", () => {
+  rect.forEach((element) => {
+    element.classList.add("fillani");
+  });
+});
+cls.addEventListener("animationend", () => {
+  path.forEach((element) => {
+    element.classList.add("fillani");
+  });
+});
+cls3.addEventListener("animationend", () => {
+  rect.forEach((element) => {
+    element.classList.add("fillani");
+  });
+});
+cls3.addEventListener("animationend", () => {
+  path.forEach((element) => {
+    element.classList.add("fillani");
+  });
+});
